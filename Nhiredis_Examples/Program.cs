e71dd0fa7d48d9bd1faa984a4fccba150ab1e462
Matt Howlett <matt.howlett@gmail.com>
2011-07-08 15:04:07 +0700

    Added examples project

---
+++ Nhiredis_Examples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Nhiredis;
+
+namespace Nhiredis_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new RedisClient("localhost", 6379, TimeSpan.FromSeconds(2));
+
+            // Send a PING command to redis using the loosly typed version of
+            // the RedisCommand function. Internally, the reply from redis is
+            // of type STATUS, with a string value of "PONG". Nhiredis returns
+            // the string value (in this case PONG), however discards the 
+            // explicit fact that the response type was STATUS. If the result
+            // of the command was ERROR, an exception would be thrown containing
+            // the error message sent from Redis.
+            object objectReply = c.RedisCommand("PING");
+            Console.WriteLine(objectReply);
+
+            // Send a PING command to redis using the strongly typed RedisCommand
+            // function. If it happened that the reply from redis can not be 
+            // reasonably interpreted as type string, an exception would be thrown.
+            string stringReply = c.RedisCommand<string>("PING");
+            Console.WriteLine(stringReply);
+
+            // Set a value in redis (ignoring the return value). Internally the
+            // reply from redis will be type status together with a string value
+            // of OK.
+            c.RedisCommand("SET", "foo", "123");
+
+            // Get a value from redis using the strongly typed version of 
+            // RedisCommand so the result is of type string, not object.
+            string str = c.RedisCommand<string>("GET", "foo");
+            Console.WriteLine(str);
+        }
+    }
+}
---
+++ Nhiredis_Examples/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nhiredis_Examples")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Nhiredis_Examples")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4cf7cb2-84e3-4b03-bd96-1a3023eb9f4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
